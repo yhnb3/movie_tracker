ce2e6422baf2dd989cdf8c56223abee59c066404
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-11-06 00:56:19 +0900

    add poster rate and toucing with svg

--- FE/src/component/contentList/contentList.jsx
+++ FE/src/component/contentList/contentList.jsx
@@ -1,36 +1,35 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RateCircle from './rateCircle';
 
-export default function NowPlaying({ content }) {
+export default function contentList({ content }) {
   const posterUrl = `https://image.tmdb.org/t/p/w300/${content.poster_path}`;
   const pathUrl = content.title ? `/movie/${content.id}` : `/tv/${content.id}`;
 
   const rate = (score) => {
-    if (score >= 8)
-      return (
-        <div className="absolute w-6 h-6 left-1 bottom-1 z-10 bg-green-200 items-center">
-          {score * 10}
-        </div>
-      );
-    if (score >= 5)
-      return (
-        <div className="absolute w-6 h-6 left-1 bottom-1 z-10 bg-blue-200 items-center">
-          {score * 10}
-        </div>
-      );
+    let color = 'green';
+
+    if (score < 7) {
+      color = 'yellow';
+    }
+
+    if (score < 4) {
+      color = 'red';
+    }
+
     return (
-      <div className="absolute w-6 h-6 left-1 bottom-1 z-10 bg-red-100 items-center">
-        {score * 10}
+      <div className="absolute w-6 h-6 left-1 bottom-1 z-10">
+        <RateCircle rate={score} color={color} />
       </div>
     );
   };
 
   return (
-    <div className="relative h-img w-img m-2 flex-col">
+    <div className="relative h-img w-img m-4 flex-col">
       <Link to={pathUrl}>
         <img
-          className="rounded object-cover w-img h-full mx-auto"
+          className="rounded-lg object-cover w-img h-full mx-auto"
           src={posterUrl}
           alt=""
         />
---
+++ FE/src/component/contentList/rateCircle.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default function rateCircle({ rate, color }) {
+  const dashArray = `${(rate * Math.PI * 2 * 17) / 10} ${
+    Math.PI * 2 * 17 - (rate * Math.PI * 2 * 17) / 10
+  }`;
+
+  console.log(rate);
+  const dashOffset = (Math.PI * 2 * 17) / 4;
+
+  console.log(dashArray, Math.PI * 17);
+  return (
+    <svg
+      version="1.1"
+      baseProfile="full"
+      width="40"
+      height="40"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="20" cy="20" r="20" fill="black" />
+      <circle
+        cx="20"
+        cy="20"
+        r="17"
+        strokeWidth="3"
+        stroke={color}
+        strokeOpacity="0.3"
+      />
+      <circle
+        cx="20"
+        cy="20"
+        r="17"
+        fill="none"
+        stroke={color}
+        strokeWidth="3"
+        strokeDasharray={dashArray}
+        strokeDashoffset={dashOffset}
+        strokeLinecap="round"
+      />
+      <text
+        x="8"
+        y="26"
+        fill="white"
+        fontSize="17"
+        fontFamily="'맑은 고딕', cursive"
+      >
+        {rate * 10}
+      </text>
+      <text
+        x="27"
+        y="18"
+        fill="white"
+        fontSize="5"
+        fontFamily="'맑은 고딕', cursive"
+      >
+        %
+      </text>
+    </svg>
+  );
+}
