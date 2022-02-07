1457cc8a24d7b2fb33ccaf1e2a702a8fe89f5ec6
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-02-07 22:40:53 +0900

    delete reducer

--- FE/src/store.js
+++ FE/src/store.js
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import movieReducer from './pages/contentSlice';
 import searchReducer from './pages/search/searchSlice';
-import personReducer from './pages/person/personSlice';
 
 export default configureStore({
   reducer: {
-    content: movieReducer,
     search: searchReducer,
-    person: personReducer,
   },
 });
